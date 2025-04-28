6665a98604cd4903ea014a8ebea07f854dd56d03
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-04-28 12:14:37 +0300

    added font-family

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -9,6 +9,16 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         <title>Top of the Tops</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Component {...pageProps} />
     </>
